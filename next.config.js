9b019a9b2b3fcc75703339424128dedfd6bb228c
003185 <003185@fcbox.com>
2020-11-09 16:04:46 +0800

    add: 学习nextjs

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 module.exports = {
   exportPathMap: function (){
     return {
-      '': {page: '/'},
-      'about': {
-        page: '/about'
-      }
+      '/': {page: '/'},
+      '/about': { page: '/about' },
     }
   }
 }
